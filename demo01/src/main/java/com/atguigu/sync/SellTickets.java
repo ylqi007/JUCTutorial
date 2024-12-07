59b2d18c040a8926a4259aae3784a83d03524ad6
ylqi007 <ylqi92121@gmail.com>
2024-12-07 11:08:08 -0800

    demo 01, 卖票程序案例

---
+++ demo01/src/main/java/com/atguigu/sync/SellTickets.java
@@ -0,0 +1,67 @@
+package com.atguigu.sync;
+
+/**
+ * Description:
+ *
+ * @Author: ylqi007
+ * @Create: 11/22/24 22:56
+ */
+
+//1.创建资源类 定义属性和操作方法
+class Ticket {
+    // 属性: 票数
+    private int number = 30;
+
+    // 操作方法: 卖票
+    public synchronized void sale() {
+        //判断是否可以卖票 数量-1
+        if (number > 0) {
+            System.out.println(Thread.currentThread().getName() + " :卖出: 1" + " 剩下: " + --number);
+        }
+    }
+}
+
+
+public class SellTickets {
+    //创建多个线程，调用资源类得操作方法
+    public static void main(String[] args) {
+        //创建Ticket对象
+        Ticket ticket = new Ticket();
+
+        //创建三个线程
+        // Thread t1
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //调用卖票得方法
+                for (int i = 0; i < 40; i++) {
+                    ticket.sale();
+                }
+            }
+        }, "AA").start();
+
+        // Thread t2
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //调用卖票得方法
+                for (int i = 0; i < 40; i++) {
+                    ticket.sale();
+                }
+            }
+        }, "BB").start();
+
+        // Thread t3
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //调用卖票得方法
+                for (int i = 0; i < 40; i++) {
+                    ticket.sale();
+                }
+            }
+        }, "CC").start();
+    }
+}
+
+
