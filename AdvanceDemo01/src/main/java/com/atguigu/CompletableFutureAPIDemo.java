d59bb7a480bfa0824c06e21ff1a8521fd5f15a71
ylqi007 <ylqi92121@gmail.com>
2025-01-11 13:11:03 -0800

    CompletableFuture常用方法：对计算结果进行处理

--- AdvanceDemo01/src/main/java/com/atguigu/CompletableFutureAPIDemo.java
+++ AdvanceDemo01/src/main/java/com/atguigu/CompletableFutureAPIDemo.java
@@ -79,4 +79,84 @@ public class CompletableFutureAPIDemo {
         // Sleep 3s, then return: false	abs, 也就是 stringCompletableFuture 没有被打断，返回的就是 CompletableFuture 计算的结果
         System.out.println(stringCompletableFuture.complete("complete value") + "\t" + stringCompletableFuture.join());
     }
+
+    /**
+     * 对计算结果进行处理
+     *  * thenApply ---> 计算结果存在依赖关系，这两个线程串行化 ----> 由于存在依赖关系（当前步错，不走下一步），当前步骤有异常的话就叫停
+     *      CompletableFuture1 --> CompletableFuture2 --> ...
+     *  * handle ---> 计算结果存在依赖关系，这两个线程串行化 ----> 有异常也可以往下走一步
+     */
+    @Test
+    public void test02ThenApply() {
+        CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Step 1: 买鱼");
+            return "Step 1: 买完鱼了";
+        }).thenApply(f -> {
+            // int i = 10 / 0;  // 当前步骤有异常，运行叫停. 不会执行后续的 thenApply，也不会执行 whenComplete，直接到 exceptionally
+            System.out.println("Step 2: 做鱼");
+            return f + "\n" + "Step 2: 做完鱼了";
+        }).thenApply(f -> {
+            // int i = 10 / 0;  // 当前步骤有异常，运行叫停. 不会执行后续的 thenApply，也不会执行 whenComplete，直接到 exceptionally
+            System.out.println("Step 3: 吃鱼");
+            return f + "\n" + "Step 3: 吃完鱼了";
+        }).whenComplete((v, e) -> {
+            if(e == null) {
+                System.out.println("吃饭结束，步骤回顾：\n" + v);
+            }
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return null;
+        });
+
+        System.out.println(Thread.currentThread().getName() + " is working on other tasks");
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void test03Handle() {
+        CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Step 1: 买鱼");
+            return "Step 1: 买完鱼了";
+        }).handle((f, e) -> {
+            // int i = 10 / 0; // 这步有异常，无法处理result1，并返回结果。但是 Step3 还可以继续
+            System.out.println("Step 2: 做鱼");
+            return f + "\n" + "Step 2: 做完鱼了";
+        }).handle((f, e) -> {
+            int i = 10 / 0;  // 这步有异常，无法处理result1，并返回结果。直接到 exceptionally
+            System.out.println("Step 3: 吃鱼");
+            return f + "\n" + "Step 3: 吃完鱼了";
+        }).whenComplete((v, e) -> {
+            if(e == null) {
+                System.out.println("吃饭结束，步骤回顾：\n" + v);
+            }
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return null;
+        });
+
+        System.out.println(Thread.currentThread().getName() + " is working on other tasks");
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
