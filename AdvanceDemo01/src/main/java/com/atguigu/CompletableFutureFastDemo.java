91f6a0e911fdbcd139d357f729ba7b1ec25eb404
ylqi007 <ylqi92121@gmail.com>
2025-01-11 13:23:18 -0800

    CompletableFuture 常用办法：对计算速度进行选用

--- AdvanceDemo01/src/main/java/com/atguigu/CompletableFutureFastDemo.java
+++ AdvanceDemo01/src/main/java/com/atguigu/CompletableFutureFastDemo.java
@@ -1,4 +1,38 @@
 package com.atguigu;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 public class CompletableFutureFastDemo {
+
+    @Test
+    public void test1() {
+        CompletableFuture<String> playerA = CompletableFuture.supplyAsync(() -> {
+            System.out.println("PlayerA is coming");
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "Player A";
+        });
+
+        CompletableFuture<String> playerB = CompletableFuture.supplyAsync(() -> {
+            System.out.println("PlayerB is coming");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "Player B";
+        });
+
+        CompletableFuture<String> stringCompletableFuture = playerA.applyToEither(playerB, f -> {
+            return f + " is winner";
+        });
+
+        System.out.println(Thread.currentThread().getName() + "----" + stringCompletableFuture.join());
+    }
 }
