6a19af45679e0efa58d5a59612398978cc4206ff
ylqi007 <ylqi92121@gmail.com>
2025-01-11 13:16:07 -0800

    CompletableFuture 常用方法：对计算结果进行处理

--- AdvanceDemo01/src/main/java/com/atguigu/CompletableFutureAPIDemo.java
+++ AdvanceDemo01/src/main/java/com/atguigu/CompletableFutureAPIDemo.java
@@ -124,7 +124,7 @@ public class CompletableFutureAPIDemo {
     }
 
     @Test
-    public void test03Handle() {
+    public void test02Handle() {
         CompletableFuture.supplyAsync(() -> {
             try {
                 TimeUnit.SECONDS.sleep(2);
@@ -159,4 +159,77 @@ public class CompletableFutureAPIDemo {
             throw new RuntimeException(e);
         }
     }
+
+    @Test
+    public void test03ProcessResults() {
+        CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("111");
+            return 1;
+        }).thenApply(f -> {
+            // int i = 10 / 0;
+            System.out.println("222");
+            return f + 2;
+        }).thenApply(f -> {
+            System.out.println("333");
+            return f + 3;
+        }).whenComplete((v, e) -> {
+            if(e == null) {
+                System.out.println("计算结果 = " + v);
+            }
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return null;
+        });
+
+        System.out.println(Thread.currentThread().getName() + " is working on other tasks");
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void test04() {
+        CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("111");
+            return 1;
+        }).handle((f, e) -> {
+            int i = 10 / 0;
+            System.out.println("222");
+            return f + 2;
+        }).handle((f, e) -> {
+            System.out.println("333");
+            return f + 3;
+        }).whenComplete((v, e) -> {
+            if(e == null) {
+                System.out.println("计算结果 = " + v);
+            }
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return null;
+        });
+
+        System.out.println(Thread.currentThread().getName() + " is working on other tasks");
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
