a5a71033491da92d23d4b3808c9c2f73641a9d6a
ylqi007 <ylqi92121@gmail.com>
2025-01-18 21:33:23 -0800

    CompletableFuture 常用方法4: 对计算速度进行选用, applyToEither

---
+++ AdvanceDemo01/src/main/java/com/atguigu/CompletableFutureAPI4Demo.java
@@ -0,0 +1,34 @@
+package com.atguigu;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class CompletableFutureAPI4Demo {
+
+    @Test
+    public void test1() {
+        CompletableFuture<String> completableFuturePlayerA = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "Player A";
+        });
+
+        CompletableFuture<String> completableFuturePlayerB = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "Player B";
+        });
+
+        CompletableFuture<String> result = completableFuturePlayerA.applyToEither(completableFuturePlayerB, f -> f + " is winner");
+
+        System.out.println(Thread.currentThread().getName() + ": " + result.join());    // without join(): main: java.util.concurrent.CompletableFuture@3e3047e6[Not completed]
+    }
+}
